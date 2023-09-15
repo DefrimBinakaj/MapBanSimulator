ca80b7d34f2de55394ddc23c64f4601bac9db957
DefrimB <defrim.binakaj@gmail.com>
2023-09-15 16:12:17 -0600

    added map ranking with toggleable type selection

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,24 +6,70 @@ using System.Windows.Controls;
 using System.Windows.Shapes;
 using System.Windows.Media.Imaging;
 using System.Diagnostics;
-using MapBanSimulator.Pages;
+using System.Windows.Navigation;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
 
 namespace MapBanSimulator;
 
 public partial class MainWindow : Window
 {
 
-    private Dictionary<string, (Rectangle redOverlay, Image cross, Image check)> mapControls;
+    enum clickMode
+    {
+        rankingMode,
+        banningMode
+    }
+
+    Dictionary<int, Color> rankColourTable = new Dictionary<int, Color>
+    {
+        {1, Colors.LawnGreen },
+        {2, Colors.GreenYellow },
+        {3, Colors.GreenYellow },
+        {4, Colors.LightGreen },
+        {5, Colors.Yellow },
+        {6, Colors.Orange },
+        {7, Colors.OrangeRed }
+    };
+
+
+    // default mode
+    clickMode currMode = clickMode.banningMode;
+
+    private Dictionary<string, (Rectangle redOverlay, Image cross, Image check)> mapBans;
+
+    private ObservableCollection<string> mapRanking;
+
+
+
+
+
+
+
+    // private static readonly HttpClient client = new HttpClient();
+
+    // private Dictionary<string, double> mapPublicBanRate;
+
+
+
+
+
+
+
 
     public MainWindow()
     {
         InitializeComponent();
-        InitBanPhase();
+        initData();
     }
 
-    private void InitBanPhase()
+
+
+    private void initData()
     {
-        mapControls = new Dictionary<string, (Rectangle, Image, Image)>
+        mapBans = new Dictionary<string, (Rectangle, Image, Image)>
         {
             {"Anubis", (anubisRedOverlay, anubisCross, anubisCheck)},
             {"Ancient", (ancientRedOverlay, ancientCross, ancientCheck)},
@@ -34,22 +80,143 @@ public partial class MainWindow : Window
             {"Nuke", (nukeRedOverlay, nukeCross, nukeCheck)}
         };
 
+        // subscribe UI to maprank update for better decoupling (instead of calling it from inside the mapClicked function as a List<string>
+        mapRanking = new ObservableCollection<string>();
+        mapRanking.CollectionChanged += mapRankCollecUpdate;
+    }
+
+
+
+    private void clickModeToggle(object sender, RoutedEventArgs e)
+    {
+        clearBans();
+        clearRanks();
+        // ternary toggle logic
+        // currMode = currMode == clickMode.rankingMode ? clickMode.banningMode : clickMode.rankingMode;
+
+
+        if (currMode == clickMode.rankingMode)
+        {
+            currMode = clickMode.banningMode;
+            mainBackground.Background = new SolidColorBrush(Colors.Maroon);
+        }
+        else if (currMode == clickMode.banningMode)
+        {
+            currMode = clickMode.rankingMode;
+            mainBackground.Background = new SolidColorBrush(Colors.LightSlateGray);
+        }
+
+    }
+
+
+
+
+
+    private void clearBans()
+    {
+        foreach (var control in mapBans)
+        {
+            control.Value.redOverlay.Visibility = Visibility.Hidden;
+            control.Value.cross.Visibility = Visibility.Hidden;
+            control.Value.check.Visibility = Visibility.Hidden;
+        }
+    }
+
+
+    private void clearRanks()
+    {
+        foreach (var mapName in mapRanking)
+        {
+            TextBlock correspondingTextBlock = getRankText(mapName);
+            correspondingTextBlock.Text = string.Empty;
+            correspondingTextBlock.Visibility = Visibility.Hidden;
+        }
+        mapRanking.Clear();
+
+    }
+
+    private void clearAll()
+    {
+        clearBans();
+        clearRanks();
     }
 
     private void MapClicked(string mapName)
     {
+        switch (currMode)
+        {
+            case clickMode.banningMode:
+                banClick(mapName);
+                break;
 
-        mapControls[mapName].redOverlay.Visibility = Visibility.Visible;
-        mapControls[mapName].cross.Visibility = Visibility.Visible;
+            case clickMode.rankingMode:
+                rankClick(mapName);
+                break;
 
-        int banCount = mapControls.Count(mpCntrl => mpCntrl.Value.redOverlay.Visibility == Visibility.Visible);
+            default:
+                Debug.WriteLine("clickMode is incorrect");
+                break;
+        }
+    }
+
+
+
+    private void banClick(string mapName)
+    {
+        mapBans[mapName].redOverlay.Visibility = Visibility.Visible;
+        mapBans[mapName].cross.Visibility = Visibility.Visible;
+
+        int banCount = mapBans.Count(mpCntrl => mpCntrl.Value.redOverlay.Visibility == Visibility.Visible);
 
         if (banCount == 6)
         {
-            string finalMap = mapControls.Last(mapCntrl => mapCntrl.Value.redOverlay.Visibility == Visibility.Hidden).Key;
-            mapControls[finalMap].check.Visibility = Visibility.Visible;
+            string finalMap = mapBans.Last(mapCntrl => mapCntrl.Value.redOverlay.Visibility == Visibility.Hidden).Key;
+            mapBans[finalMap].check.Visibility = Visibility.Visible;
         }
     }
+
+    private void rankClick(string mapName)
+    {
+        if (!mapRanking.Contains(mapName))
+        {
+            mapRanking.Add(mapName);
+        }
+        else if (mapRanking.Contains(mapName))
+        {
+            mapRanking.Remove(mapName);
+        }
+
+    }
+
+
+    private TextBlock getRankText(string mapName)
+    {
+        return (TextBlock)this.FindName(mapName.ToLower() + "Rank");
+    }
+
+
+
+
+    private void mapRankCollecUpdate(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+    {
+        foreach (var mapName in mapBans.Keys)
+        {
+            getRankText(mapName).Visibility = Visibility.Hidden;
+        }
+
+        for (int i = 0; i < mapRanking.Count; i++)
+        {
+            string mapName = mapRanking[i];
+            TextBlock correspondingTextBlock = getRankText(mapName);
+            correspondingTextBlock.Text = (i + 1).ToString();
+            correspondingTextBlock.Visibility = Visibility.Visible;
+            correspondingTextBlock.Foreground = new SolidColorBrush(rankColourTable[i + 1]);
+        }
+    }
+
+
+
+
 
 
 
@@ -60,24 +227,12 @@ public partial class MainWindow : Window
     private void mirageClicked(object sender, RoutedEventArgs e) => MapClicked("Mirage");
     private void overpassClicked(object sender, RoutedEventArgs e) => MapClicked("Overpass");
     private void vertigoClicked(object sender, RoutedEventArgs e) => MapClicked("Vertigo");
-
-    private void refreshClicked(object sender, RoutedEventArgs e)
-    {
-        foreach (var control in mapControls)
-        {
-            control.Value.redOverlay.Visibility = Visibility.Hidden;
-            control.Value.cross.Visibility = Visibility.Hidden;
-            control.Value.check.Visibility = Visibility.Hidden;
-        }
-    }
-
-
-    private void navCalcPage(object sender, RoutedEventArgs e)
-    {
-        // mainFrame.Content = new CalcPage();
-        // mainFrame.NavigationService.Navigate(new CalcPage());
-    }
-
-
+    
+    private void refreshClicked(object sender, RoutedEventArgs e) => clearAll();
+    
+    private void refreshHover(object sender, RoutedEventArgs e) => refreshButton.Opacity = 0.6;
+    private void refreshUnhover(object sender, RoutedEventArgs e) => refreshButton.Opacity = 0.8;
+    private void refreshMouseDown(object sender, RoutedEventArgs e) => refreshButton.Opacity = 1.0;
+    private void refreshMouseUp(object sender, RoutedEventArgs e) => refreshButton.Opacity = 0.8;
 }
 
